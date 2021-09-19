4e3479fd028347f07400f12ad45a87680eaa73f5
casodio <marciojesusmaia@hotmail.com>
2021-09-19 19:06:35 -0300

    Adicionado retorna lista por json, retorno de http404 e configurado timezone para pt-br

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime, timedelta
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length= 100, verbose_name='Título')
@@ -14,3 +14,12 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
